64acd1be33be00bedd74e5e741146333183d0217
Joelle Maslak <jmaslak@antelope.net>
2023-12-10 19:02:34 -0700

    Add solution in Golang

---
+++ 10/10.go
@@ -0,0 +1,166 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type DistNode struct {
+	distance int
+	coord    Coord
+}
+
+func main() {
+	var t Table[rune]
+	row := 0
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		line := scanner.Text()
+		cols := []rune(line)
+		for col, v := range cols {
+			t.PutXY(row, col, v)
+			col++
+		}
+		row++
+	}
+	t.AddBorder('.')
+
+	max := -1
+
+	// Find "S" and initialize dist
+	var start Coord
+	var dist Table[int]
+	for row = 0; row < t.RowCount(); row++ {
+		for col := 0; col < t.ColCount(); col++ {
+			if t.GetXY(row, col) == 'S' {
+				start = Coord{row: row, col: col}
+				dist.Put(start, 0)
+			} else {
+				dist.PutXY(row, col, max)
+			}
+		}
+	}
+
+	stack := make([]DistNode, 0, 0)
+	up := t.Get(start.N())
+	if up == '|' || up == '7' || up == 'F' {
+		stack = append(stack, DistNode{distance: 1, coord: start.N()})
+	}
+	dn := t.Get(start.S())
+	if dn == '|' || dn == 'L' || dn == 'J' {
+		stack = append(stack, DistNode{distance: 1, coord: start.S()})
+	}
+	lt := t.Get(start.W())
+	if lt == '-' || lt == 'L' || lt == 'F' {
+		stack = append(stack, DistNode{distance: 1, coord: start.W()})
+	}
+	rt := t.Get(start.E())
+	if rt == '-' || rt == '7' || rt == 'J' {
+		stack = append(stack, DistNode{distance: 1, coord: start.E()})
+	}
+
+	for len(stack) > 0 {
+		v := stack[0]
+		stack = stack[1:]
+
+		if dist.Get(v.coord) == -1 || v.distance < dist.Get(v.coord) {
+			dist.Put(v.coord, v.distance)
+			char := t.Get(v.coord)
+			d := v.distance + 1
+			if char == '-' {
+				stack = append(stack, DistNode{distance: d, coord: v.coord.W()})
+				stack = append(stack, DistNode{distance: d, coord: v.coord.E()})
+			} else if char == '|' {
+				stack = append(stack, DistNode{distance: d, coord: v.coord.N()})
+				stack = append(stack, DistNode{distance: d, coord: v.coord.S()})
+			} else if char == 'J' {
+				stack = append(stack, DistNode{distance: d, coord: v.coord.N()})
+				stack = append(stack, DistNode{distance: d, coord: v.coord.W()})
+			} else if char == '7' {
+				stack = append(stack, DistNode{distance: d, coord: v.coord.S()})
+				stack = append(stack, DistNode{distance: d, coord: v.coord.W()})
+			} else if char == 'F' {
+				stack = append(stack, DistNode{distance: d, coord: v.coord.E()})
+				stack = append(stack, DistNode{distance: d, coord: v.coord.S()})
+			} else if char == 'L' {
+				stack = append(stack, DistNode{distance: d, coord: v.coord.E()})
+				stack = append(stack, DistNode{distance: d, coord: v.coord.N()})
+			}
+		}
+	}
+
+	steps := 0
+	for row = 0; row < t.RowCount(); row++ {
+		for col := 0; col < t.ColCount(); col++ {
+			if dist.GetXY(row, col) > steps {
+				steps = dist.GetXY(row, col)
+			}
+		}
+	}
+
+	var expanded Table[rune]
+	for row = 0; row < dist.RowCount(); row++ {
+		for col := 0; col < dist.ColCount(); col++ {
+			expanded.PutXY(row*2, col*2, ' ')
+			expanded.PutXY(row*2, col*2+1, ' ')
+			expanded.PutXY(row*2+1, col*2, ' ')
+			expanded.PutXY(row*2+1, col*2+1, ' ')
+			if dist.GetXY(row, col) >= 0 {
+				expanded.PutXY(row*2, col*2, t.GetXY(row, col))
+				char := t.GetXY(row, col)
+				if char == '-' {
+					expanded.PutXY(row*2, col*2-1, '-')
+					expanded.PutXY(row*2, col*2+1, '-')
+				} else if char == '|' {
+					expanded.PutXY(row*2-1, col*2, '|')
+					expanded.PutXY(row*2+1, col*2, '|')
+				} else if char == 'J' {
+					expanded.PutXY(row*2-1, col*2, '|')
+					expanded.PutXY(row*2, col*2-1, '-')
+				} else if char == '7' {
+					expanded.PutXY(row*2, col*2-1, '-')
+					expanded.PutXY(row*2+1, col*2, '|')
+				} else if char == 'F' {
+					expanded.PutXY(row*2+1, col*2, '|')
+					expanded.PutXY(row*2, col*2+1, '-')
+				} else if char == 'L' {
+					expanded.PutXY(row*2-1, col*2, '|')
+					expanded.PutXY(row*2, col*2+1, '-')
+				}
+			}
+		}
+	}
+
+	stack = append(stack, DistNode{coord: Coord{row: 0, col: 0}})
+	inside := 0
+	for len(stack) > 0 {
+		v := stack[0]
+		stack = stack[1:]
+
+		row = v.coord.Row()
+		col := v.coord.Col()
+		if row < 0 || row > expanded.RowCount() || col < 0 || col > expanded.ColCount() {
+			// Do nothing, out of bounds
+		} else if expanded.Get(v.coord) == ' ' {
+			expanded.Put(v.coord, 'O')
+
+			stack = append(stack, DistNode{coord: v.coord.N()})
+			stack = append(stack, DistNode{coord: v.coord.S()})
+			stack = append(stack, DistNode{coord: v.coord.W()})
+			stack = append(stack, DistNode{coord: v.coord.E()})
+		}
+	}
+
+	inside = 0
+	for row = 0; row < expanded.RowCount(); row = row + 2 {
+		for col := 0; col < expanded.ColCount(); col = col + 2 {
+			if expanded.GetXY(row, col) == ' ' {
+				inside++
+			}
+		}
+	}
+
+	fmt.Printf("Steps: %d\n", steps)
+	fmt.Printf("Inside: %d\n", inside)
+}
---
+++ 10/table.go
@@ -0,0 +1,141 @@
+package main
+
+import "fmt"
+
+type Coord struct {
+	row int
+	col int
+}
+
+type Row[T any] struct {
+	cols []T
+}
+
+type Table[T any] struct {
+	rows []Row[T]
+}
+
+func (t Table[T]) RowCount() int {
+	return len(t.rows)
+}
+
+func (t Table[T]) ColCount() int {
+	max := 0
+	for _, row := range t.rows {
+		if max < len(row.cols) {
+			max = len(row.cols)
+		}
+	}
+	return max
+}
+
+func (t Table[T]) Get(coord Coord) T {
+	row := coord.row
+	col := coord.col
+
+	return t.GetXY(row, col)
+}
+
+func (t Table[T]) GetXY(row, col int) T {
+	if len(t.rows) > row {
+		if len(t.rows[row].cols) > col {
+			return t.rows[row].cols[col]
+		}
+	}
+	var n T
+	return n
+}
+
+func (t *Table[T]) Put(coord Coord, node T) {
+	row := coord.row
+	col := coord.col
+
+	t.PutXY(row, col, node)
+}
+
+func (t *Table[T]) PutXY(row, col int, node T) {
+	for len(t.rows) <= row {
+		var r Row[T]
+		r.cols = make([]T, col, col)
+		t.rows = append(t.rows, r)
+	}
+	for len(t.rows[row].cols) <= col {
+		var n T
+		t.rows[row].cols = append(t.rows[row].cols, n)
+	}
+	t.rows[row].cols[col] = node
+}
+
+func (t *Table[T]) AddBorder(node T) {
+	rows := t.RowCount()
+	cols := t.ColCount()
+
+	newRows := make([]Row[T], rows+2, rows+2)
+	copy(newRows[1:], t.rows)
+
+	// Fill in top an dbottom
+	newRows[0].cols = make([]T, cols+2, cols+2)
+	newRows[len(newRows)-1].cols = make([]T, cols+2, cols+2)
+	for i := 0; i < cols+2; i++ {
+		newRows[0].cols[i] = node
+		newRows[len(newRows)-1].cols[i] = node
+	}
+
+	// Copy in old data and add left/right borders
+	for i := 0; i < rows; i++ {
+		newCol := make([]T, cols+2, cols+2)
+		copy(newCol[1:], t.rows[i].cols)
+		newCol[0] = node
+		newCol[len(newCol)-1] = node
+		newRows[i+1].cols = newCol
+	}
+
+	t.rows = newRows
+}
+
+func (t Table[T]) Print(format string) {
+	for row := 0; row < t.RowCount(); row++ {
+		for col := 0; col < t.ColCount(); col++ {
+			v := t.Get(Coord{col: col, row: row})
+			fmt.Printf(format, v)
+		}
+		fmt.Println("")
+	}
+}
+
+func (c Coord) N() Coord {
+	return Coord{row: c.row - 1, col: c.col}
+}
+
+func (c Coord) S() Coord {
+	return Coord{row: c.row + 1, col: c.col}
+}
+
+func (c Coord) W() Coord {
+	return Coord{row: c.row, col: c.col - 1}
+}
+
+func (c Coord) E() Coord {
+	return Coord{row: c.row, col: c.col + 1}
+}
+
+func (c Coord) Print() {
+	fmt.Printf("%d, %d\n", c.row, c.col)
+}
+
+func (c Coord) OOB() bool {
+	// Verifies the coordinates aren't out of bounds (important
+	// after a N/S/E/W() operation)
+	if c.row < 0 || c.col < 0 {
+		return true
+	}
+	return false
+}
+
+func (c Coord) Row() int {
+	return c.row
+}
+
+func (c Coord) Col() int {
+	return c.col
+}
