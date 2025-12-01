00cdc03ef3aa475481fdcff01fc72523d7ca32df
Joelle Maslak <jmaslak@antelope.net>
2025-11-30 22:51:29 -0700

    Solution to 2025-01

---
+++ 2025/01/01.pl
@@ -0,0 +1,67 @@
+#!/usr/bin/perl
+
+#
+# Copyright (C) 2025 Joelle Maslak
+# All Rights Reserved - See License
+#
+
+use JTM::Boilerplate 'script';
+
+MAIN: {
+    my @dial;
+    while ( <<>> ) {
+        chomp;
+        my $dir    = substr( $_, 0, 1 );
+        my $clicks = substr( $_, 1 );
+
+        if ( $dir eq 'L' ) {
+            push @dial, -$clicks;
+        } else {
+            push @dial, +$clicks;
+        }
+    }
+
+    my $dial  = 50;
+    my $part1 = 0;
+    my $part2 = 0;
+
+    for my $spin (@dial) {
+        my $old = $dial;
+
+        # If we have > 100 clicks, we'll want to normalize this, but
+        # also count for part 2!
+        my $hundreds += int(abs($spin)/100);
+        $part2 += $hundreds;
+
+        if ($spin < 100) {
+            # Left
+            $spin = $spin + ($hundreds*100);
+        } else {
+            # Right
+            $spin %= 100;
+        }
+
+        # Spin -- note this will leave dial negative for a bit.
+        $dial = ( $dial + $spin );
+
+        # Do we need to increment counts?
+        if ( !( $dial % 100 ) ) {
+            # We end on zero, so we increment.
+            $part1++;
+            $part2++;
+        } else {
+            if ( $old != 0 ) {
+                # We started on zero, so going negative isn't going
+                # through zero again, so we don't count it.
+                if ( ( $dial < 0 ) || ( $dial > 100 ) ) {
+                    $part2++;
+                }
+            }
+        }
+        $dial %= 100;
+    }
+
+    say( "Part 1: ", $part1 );
+    say( "Part 2: ", $part2 );
+}
+
