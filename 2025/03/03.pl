3e6be51d1ee2afedfa3a248f0bc5892ea17f2169
Joelle Maslak <jmaslak@antelope.net>
2025-12-02 23:44:51 -0700

    Fix performance issue

--- 2025/03/03.pl
+++ 2025/03/03.pl
@@ -8,7 +8,8 @@
 use lib '.';
 
 use JTM::Boilerplate 'script';
-use List::Util qw(max uniqstr);
+use List::Util qw(max sum);
+use Parallel::WorkUnit;
 
 MAIN: {
     my @strings;
@@ -22,8 +23,11 @@ MAIN: {
 
     for my $string (@strings) {
         $part1 += largest($string, 2);
-        $part2 += largest($string, 12);
     }
+    
+    my $wu = Parallel::WorkUnit->new();
+    $wu->queueall(\@strings, sub($x) { largest($x, 12) });
+    $part2 = sum($wu->waitall());
 
     say("Part 1: $part1");
     say("Part 2: $part2");
@@ -39,21 +43,12 @@ sub largest($string, $sz) {
     my @possibles = @$string[0..$lastidx];
     my $max = max(@possibles);
 
-    my $ret = 0;
-    my %seen;  # Used as speedup.
     for (my $i=0; $i<=$lastidx; $i++) {
         my $val = $string->[$i];
         if ($val != $max) { next }
         my (@remainder) = @$string[($i+1)..($len-1)];
-        if (scalar(@remainder)) {
-            if (exists $seen{$val . $remainder[0]}) { next }
-            $seen{$val . $remainder[0]} = 1;
-        }
-        my $x = "$val" . largest(\@remainder, $sz-1);
-        if ($x > $ret) {
-            $ret = $x;
-        }
+        return "$val" . largest(\@remainder, $sz-1);
     }
 
-    return $ret
+    die("shouldn't get here");
 }
