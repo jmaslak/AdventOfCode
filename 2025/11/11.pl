b560167e3ff9bb23a434cbc90abf011b992fa6d0
Joelle Maslak <jmaslak@antelope.net>
2025-12-10 23:48:47 -0700

    Tidy

--- 2025/11/11.pl
+++ 2025/11/11.pl
@@ -9,35 +9,34 @@ use JTM::Boilerplate 'script';
 use List::Util qw(any sum);
 use Memoize;
 
-sub traverse($exits, $src="you", $out="out", @must) {
+sub traverse( $exits, $src = "you", $out = "out", @must ) {
     @must = grep { $_ ne $src } @must;
 
-    if ($src eq $out) {
-        if (scalar(@must)) {
+    if ( $src eq $out ) {
+        if ( scalar(@must) ) {
             return 0;
         } else {
             return 1;
         }
     }
 
-    return sum map { traverse($exits, $_, $out, @must) } $exits->{$src}->@*;
+    return sum map { traverse( $exits, $_, $out, @must ) } $exits->{$src}->@*;
 }
 memoize('traverse');
 
 MAIN: {
     my %exits;
-    for (<<>>) {
+    for ( <<>> ) {
         chomp;
         next if (/^$/);
 
-        my ($source, $dst) = split /: /;
+        my ( $source, $dst ) = split /: /;
         my (@dsts) = split / /, $dst;
 
         $exits{$source} = \@dsts;
     }
 
-    say "Part 1: " . traverse(\%exits, "you", "out");
-    say "Part 2: " . traverse(\%exits, "svr", "out", "fft", "dac");
+    say "Part 1: " . traverse( \%exits, "you", "out" );
+    say "Part 2: " . traverse( \%exits, "svr", "out", "fft", "dac" );
 }
-
 
